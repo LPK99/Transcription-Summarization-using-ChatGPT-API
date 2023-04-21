e700a4fb376514d7dcfdcf2c2b49b5376bb08bbc
Your Name <you@example.com>
2023-04-20 19:13:40 -0700

    added gradio ui

--- different_prompt_function.py
+++ different_prompt_function.py
@@ -1,45 +1,45 @@
 import whisper
 import openai
 import gradio as gr
-import numpy as np
+import api_key
 
-
+openai.api_key = api_key.OPEN_AI_API_KEY
 
 def transcribe(filename):
     model = whisper.load_model("base")
-    #print(filename)
-    #audio = whisper.load_audio(filename)
-    #convert_to_array = np.array(filename, dtype=np.int16)
-    #print(convert_to_array)
-    result = model.transcribe(filename[1])
+    result = model.transcribe(filename)
     return(result["text"])
 
 def short_summarize(result):
+    content = transcribe(result)
+    print
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
             {"role": "system", "content": "You are a professor . Summarize to all inputs in 50 concise words or less"},
-            {"role": "user", "content": result["text"]},
+            {"role": "user", "content": content},
         ]
     )
     return response["choices"][0]["message"]["content"]
 
 def long_summarize(result):
+    content = transcribe(result)
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
             {"role": "system", "content": "You are a professor . Summarize to all inputs in a short and concise paragraph"},
-            {"role": "user", "content": result["text"]},
+            {"role": "user", "content": content},
         ]
     )
     return response["choices"][0]["message"]["content"]
 
 def bulletpoints_summarize(result):
+    content = transcribe(result)
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
             {"role": "system", "content": "You are a professor . Summarize to all inputs in bullet points"},
-            {"role": "user", "content": result["text"]},
+            {"role": "user", "content": content},
         ]
     )
     return response["choices"][0]["message"]["content"]
@@ -47,11 +47,17 @@ def bulletpoints_summarize(result):
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=1, min_width=600):
-            audio_input = gr.Audio()
-            transcribe_button = gr.Button("Submit")
+            audio_input = gr.Audio(type="filepath")
+            transcribe_button = gr.Button("Transcribe")
+            short_summarize_button = gr.Button("Short Audio Summary")
+            long_summarize_button = gr.Button("Long Audio Summary")
+            bulletpoints_summarize_button = gr.Button("Bullet Points Summary")
             text_output = gr.Textbox()
 
     transcribe_button.click(transcribe, inputs=audio_input, outputs=text_output)
+    short_summarize_button.click(short_summarize, inputs=audio_input, outputs=text_output)
+    long_summarize_button.click(long_summarize, inputs=audio_input, outputs=text_output)
+    bulletpoints_summarize_button.click(bulletpoints_summarize, inputs=audio_input, outputs=text_output)
 
 demo.launch()
 
