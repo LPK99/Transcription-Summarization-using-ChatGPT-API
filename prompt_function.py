5be7c4f6991765b1fd8c4821dedbdb97d269d2bd
Your Name <you@example.com>
2023-04-21 14:39:39 -0700

    added comment and text feature

--- prompt_function.py
+++ prompt_function.py
@@ -2,8 +2,8 @@ import whisper
 import openai
 import api_key
 
-
-
+#take the prompt and text content as parameter
+#call the chatgpt api and return the response
 def request_gpt(prompt, text):
     openai.api_key = api_key.OPEN_AI_API_KEY
     content = transcribe(text)
@@ -12,13 +12,17 @@ def request_gpt(prompt, text):
         model='gpt-3.5-turbo',
         messages=[{"role": "system", "content": prompt}, {"role": "user", "content": content}]
     )
-    print(response["choices"][0]["message"]["content"])
     return response["choices"][0]["message"]["content"]
-
+#transcribe the file into text
 def transcribe(filename):
-    model = whisper.load_model("base")
-    result = model.transcribe(filename)
-    return(result["text"])
+    if type(filename) == str: #take the text if the parameter is already a text
+        return filename
+    else:
+        model = whisper.load_model("base")
+        result = model.transcribe(filename)
+        return(result["text"])
+
+#declare the prompt and call the request_gpt function
 
 def short_summarize(result):
     return request_gpt('You are a professor . Summarize to all inputs in 50 concise words or less', result)
